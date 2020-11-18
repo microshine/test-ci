8d7e473a5aa9c6d13cc240b4ce94cad980b7d016
microshine <microshine@mail.ru>
2020-11-18 18:00:12 +0300

    try input usage

--- .github/actions/signer/src/index.ts
+++ .github/actions/signer/src/index.ts
@@ -1,7 +1,9 @@
 import *  as core from '@actions/core';
 
 async function main() {
+  const filePath = core.getInput("file-path");
   console.log("Message from custom action");
+  console.log("File path:", filePath);
 }
 
 main()
